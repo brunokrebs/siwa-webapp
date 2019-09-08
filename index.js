6652db3fe7f17b2c6ac956ec856aafcd7074c5da
Bruno Krebs <bruno.krebs@auth0.com>
2019-09-07 21:27:09 -0300

    apple verification

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const fs = require("fs");
 const jwt = require("jsonwebtoken");
 const Auth0Strategy = require('passport-auth0');
 
-const port = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(
@@ -17,21 +15,18 @@ app.use(
   })
 );
 
-app.use(passport.initialize());
-app.use(passport.session());
-
 const strategy = new Auth0Strategy(
   {
-    domain: "login.brunokrebs.com",
+    domain: "login.digituz.com.br",
     clientID: 'sdsYQI5cA6HkV4XJoUqITL6Hf4fzx5HD',
     clientSecret: 'UQxnfpvcxz8gBcTP-9fWPpjprG122_gO8sMwK67HgsUBCpo2KT8Vth5vKKABEBby',
-    callbackURL: "https://brunokrebs.com/callback"
+    callbackURL: "https://digituz.com.br/callback"
   },
   function(accessToken, refreshToken, extraParams, profile, done) {
     /**
-     * Access tokens are used to authorize users to an API 
+     * Access tokens are used to authorize users to an API
      * (resource server)
-     * accessToken is the token to call the Auth0 API 
+     * accessToken is the token to call the Auth0 API
      * or a secured third-party API
      * extraParams.id_token has the JSON Web Token
      * profile has all the information from the user
@@ -40,24 +35,9 @@ const strategy = new Auth0Strategy(
   }
 );
 
-app.use(strategy);
-
-passport.use(
-  "apple",
-  new OAuth2Strategy(
-    {
-      authorizationURL: "https://appleid.apple.com/auth/authorize",
-      tokenURL: "https://appleid.apple.com/auth/token",
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: process.env.CALLBACK,
-      state: Date.now() // bleh
-    },
-    (accessToken, refreshToken, payload, profile, done) => {
-      done(null, { profile, payload });
-    }
-  )
-);
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -67,9 +47,18 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
+const domainAssociation = fs.readFileSync(
+  "./apple-developer-domain-association.txt",
+  "utf8"
+);
+
+app.get("/.well-known/apple-developer-domain-association.txt", (req, res) => {
+  res.send(domainAssociation);
+});
+
 app.get(
 	'/login',
-	passport.authenticate('auth0', {scope: 'openid email profile'}), 
+	passport.authenticate('auth0', {scope: 'openid email profile'}),
 	function (req, res) {
 		res.redirect('/');
 	}
@@ -90,10 +79,9 @@ app.get("/profile", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  console.log("User", req.user);
-  res.send(JSON.stringify({ Hello: "World" }));
+  res.send('<a href="/login">Login</a>');
 });
 
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log(`Apple Login POC listening on port ${port}!`);
 });
